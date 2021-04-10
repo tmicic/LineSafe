8d5b2cee26a2c3aed6298fd43605f083399ba243
Thomas Micic <thomasmicic@gmail.com>
2021-04-10 16:20:49 +0100

    update and simplify datasets

--- custom_dataset.py
+++ custom_dataset.py
@@ -11,79 +11,40 @@ import numpy as np
 import os
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+import random
+import matplotlib.pyplot as plt
 
-class LineSafeDataset(VisionDataset):
+class UnbalancedDataset(VisionDataset):
 
     def __init__(self, df_path: str, 
                         root: str, 
-                        ng_roi_root: str=None, 
-                        hilar_roi_root: str=None, 
-                        return_what='all', 
                         transform=None, 
                         target_transform=None,
-                        loader=dicom_processing.auto_loader,
-                        target_loader=None) -> None:
-        '''
-
-        return_what: all = will return all images,
-                     ng = will return images only if they have an ng map
-                     hilar = will return images only if they have a hilar map
-                     both = will return images only if they have hilar and ng map
-                     ng_or_no_ng = will return images if they have an ng map or is a film that doesnt have an ng
-        '''
+                        loader=dicom_processing.auto_loader) -> None:
 
         super().__init__(root, transforms=None, transform=transform, target_transform=target_transform)
 
-        self.return_what = return_what
         self.df_path = df_path
-        self.ng_roi_root = ng_roi_root
-        self.hilar_roi_root = hilar_roi_root
         self.df = pd.read_feather(df_path)
         self.loader = loader
-        self.target_loader = target_loader
         del self.df['index']
 
 
         self.classes = sorted(self.df['category'].unique())                 # make sure that training and validation set have same classes!!! in same order!!
         self.class_to_id = {x:i for i,x in enumerate(self.classes)}
 
-        if self.return_what == 'all':
-            pass
-        elif self.return_what == 'ng':
-            self.df = self.df[self.df['ng_roi_filename'].notnull()]
-        elif self.return_what == 'hilar':
-            self.df = self.df[self.df['hilar_roi_filename'].notnull()]
-        else:
-            raise NotImplementedError()
-
     def __getitem__(self, index: int):
         
         item = self.df.iloc[index]
+
         X = self.loader(os.path.join(self.root, item['category'],item['filename']))
         if self.transform is not None: X = self.transform(X)
-
-        if self.return_what=='all':
             
-            y = torch.tensor(self.class_to_id[item['category']])
-            
-            
-            if self.target_transform is not None: y = self.target_transform(y)
+        y = torch.tensor(self.class_to_id[item['category']])
+        
+        if self.target_transform is not None: y = self.target_transform(y)
 
-            return X, y
-
-        elif self.return_what == 'ng':
-
-            y = self.target_loader(os.path.join(self.ng_roi_root,item['ng_roi_filename']))
-
-
-            if self.target_transform is not None: y = self.target_transform(y)
-
-            cat = torch.tensor(self.class_to_id[item['category']])
-
-            return X, y, cat
-
-        else:
-            raise NotImplementedError()   
+        return X, y
 
     def get_class_ratios(self):
         # returns the ratio of each class in the order of class_to_id
@@ -93,31 +54,119 @@ class LineSafeDataset(VisionDataset):
 
         return rs / rs.sum()
 
-
-        
-
     def __len__(self) -> int:
         return len(self.df)
 
-if __name__ == '__main__':
-    print('debugging')
+class UnbalancedSelfSupervisedRotationalDataset(UnbalancedDataset):
+    # 0 is 0 deg, 1 is 90 deg, 2 is 180 deg, 3 is 270 deg 
 
-    d = LineSafeDataset(common.TRAIN_DF_PATH, 
-                        root=common.SATO_IMAGES_ROOT_PATH, 
-                        ng_roi_root=common.NG_ROI_ROOT_PATH,
+    def __init__(self, df_path: str, 
+                        root: str, 
+                        transform=None, 
+                        target_transform=None,
+                        loader=dicom_processing.auto_loader) -> None:
+        super().__init__(df_path, root, transform=transform, target_transform=target_transform, loader=loader)
+
+    def __getitem__(self, index: int):
+        
+        item = self.df.iloc[index]
+
+        X = self.loader(os.path.join(self.root, item['category'],item['filename']))
+        if self.transform is not None: X = self.transform(X)
+
+        rot = torch.tensor(random.randint(0,4))
+
+        y = X.rot90(k=rot.item(), dims=(1,2))
+
+        cat = torch.tensor(self.class_to_id[item['category']])
+        
+        if self.target_transform is not None: y = self.target_transform(y)
+
+        return X, y, rot, cat
+
+class UnetDataset(UnbalancedDataset):
+
+    def __init__(self, df_path: str, 
+                        root: str,
+                        map_root: str,
+                        transform=None, 
+                        target_transform=None,
                         loader=dicom_processing.auto_loader,
                         target_loader=dicom_processing.segmentation_image_loader,
+                        allow_non_segmentated_images=True,      # if true, will return ng maps and films without ng tubes with blank maps
+                        blank_map_dims=(512,512)) -> None:      # before any transforms
+        super().__init__(df_path, root, transform=transform, target_transform=target_transform, loader=loader)
+
+
+        self.map_root = map_root
+        self.target_loader = target_loader
+        self.allow_non_segmentated_images = allow_non_segmentated_images
+        self.blank_map_dims = blank_map_dims
+
+        if not self.allow_non_segmentated_images:
+            self.df = self.df[self.df['ng_roi_filename'].notnull()]
+        else:
+            #allow no ng tube and ng tubes with ng_roi_filename
+            self.df = self.df[(self.df['ng_roi_filename'].notnull() | (self.df['category'] == 'NO_NG'))]
+
+    def __getitem__(self, index: int):
+        
+        item = self.df.iloc[index]
+
+        X = self.loader(os.path.join(self.root, item['category'],item['filename']))
+        if self.transform is not None: X = self.transform(X)
+        
+        cat = torch.tensor(self.class_to_id[item['category']])
+
+        if item['ng_roi_filename'] is None:
+            # return blank target
+            y = np.zeros(self.blank_map_dims)
+        else:
+            y = self.target_loader(os.path.join(self.map_root,item['ng_roi_filename']))
+
+        if self.target_transform is not None: y = self.target_transform(y)
+
+        return X, y, cat
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    print('debugging')
+    d = UnetDataset(common.TRAIN_DF_PATH, 
+                        root=common.SATO_IMAGES_ROOT_PATH, 
+                        map_root=common.NG_ROI_ROOT_PATH,
+                        loader=dicom_processing.auto_loader,
                         transform=transforms.Compose([
                             ToTensor()
                         ]),
                         target_transform=transforms.Compose([
                             ToTensor()
-                        ]),
-                        return_what='all')
+                        ])
+                        )
 
-    print(d.class_to_id)
-    print(d.get_class_ratios())
-    exit()
+    for r in range(0, 100):
+        x, y, cat = d.__getitem__(r)
+
+        print(x.shape)
+        print(y.shape)
+        print(cat)
+        print()
+
+        plt.subplot(1,2,1)
+        plt.imshow(x[0])
+        plt.subplot(1,2,2)
+        plt.imshow(y[0])
+
+        plt.show()
+
+
+
+
 
 
 
